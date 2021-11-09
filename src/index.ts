3cbe873ee448044f42d070321f7f36bc2476f406
hwany93 <rakddh@gmail.com>
2021-11-09 21:55:20 +0900

    html send파일 추가

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,16 @@ import "reflect-metadata";
 import {createConnection} from "typeorm";
 import {User} from "./entity/User";
 import * as console from "console";
+import * as path from "path";
 
 const express = require("express");
 const app = express();
 
 app.set("port", process.env.PORT || 3000)
 app.get("/", (req,res) => {
-    res.send("hello express");
+    res.sendfile(path.join(__dirname, "index.html"));
 })
 
 app.listen(app.get("port"), () => {
-    console.log("익스프레스 서버 실행");
+    console.log("익스프레스 서버 실행!");
 });
