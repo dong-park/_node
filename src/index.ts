f8989a00c9fed43a13eba1357c074324ffc44d6e
hwany93 <rakddh@gmail.com>
2021-11-09 22:26:49 +0900

    에러및 404 미들웨어 추가

--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,44 @@ app.use((req, res, next) => {
     next();
 })
 
-app.set("port", process.env.PORT || 3000)
 app.get("/", (req,res) => {
     res.sendfile(path.join(__dirname, "index.html"));
 })
 
+app.get("/users", (req, res) => {
+    res.send({
+        response: "hi"
+    })
+})
 
+app.get("/users/:id", (req, res) => {
+    res.send( {
+        response: `${req.params.id}번째 손님입니다.`
+    })
+})
+
+// throw exception
+app.get("/error", () => {
+    throw new Error("this is error message");
+})
+
+// app.get("*", (req, res) => {
+//     res.send({
+//         response: "기타 페이지 입니다."
+//     })
+// })
+
+app.use((req, res, next) => {
+    res.status(404).send("404 에러가 발생했습니다.")
+})
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.send("에러가 발생했습니다.")
+})
+
+
+app.set("port", process.env.PORT || 3000)
 app.listen(app.get("port"), () => {
-    console.log("익스프레스 서버 실행!!");
+    console.log("익스프레스 서버가 실행되었습니다.");
 });
